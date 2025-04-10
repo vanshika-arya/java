2fcdc9b2e6f1fe37c2e5e589c748b9dfd091143d
vanshika-arya <vanshika3915@gmail.com>
2025-04-10 21:12:18 +0530

    newly added

---
+++ uppercase.java
@@ -0,0 +1,11 @@
+public class uppercase {
+    public static void main(String[] args) {
+        char ch='j';
+        if(ch>='A' && ch<='Z')
+        System.out.println("Uppercase");
+        if(ch>='a' && ch<='z')
+        System.out.println("Lowercase");
+
+    }
+    
+}
