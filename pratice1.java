9e049856af8db1031b8cc002212a18b2f49249e8
vanshika-arya <vanshika3915@gmail.com>
2025-04-13 18:26:09 +0530

    pratice question class and object

---
+++ pratice1.java
@@ -0,0 +1,11 @@
+class student{
+
+}
+public class pratice1 {
+    public static void main(String[] args) {
+        student s= new student();
+        System.out.println(s);
+        
+    }
+    
+}
