34b329cf5d87da9217a84cce23d91b6aecb6ec3e
vanshika-arya <vanshika3915@gmail.com>
2025-04-15 13:22:31 +0530

    pattern

---
+++ pattern4.java
@@ -0,0 +1,19 @@
+public class pattern4 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=5;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            for(int m=2;m<=i;m++){
+            System.out.print("*");
+            }
+            System.out.println();
+
+
+        }
+    }
+    
+}
