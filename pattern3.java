3ad7c0bcb5b7a4e8593f34def1894cf0e18c05b4
vanshika-arya <vanshika3915@gmail.com>
2025-04-15 13:09:45 +0530

    Pattern questions

---
+++ pattern3.java
@@ -0,0 +1,15 @@
+public class pattern3 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=5;k>=i;k--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
