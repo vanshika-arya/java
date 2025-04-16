735d83507476c4e1c492122e5ad9e92c14d56306
vanshika-arya <vanshika3915@gmail.com>
2025-04-16 14:09:44 +0530

    reverse of array

---
+++ reversearray.java
@@ -0,0 +1,9 @@
+public class reversearray {
+    public static void main(String[] args) {
+        int arr[]={23,49,45,38,23};
+        for(int i=arr.length-1; i>=0; i--){
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
