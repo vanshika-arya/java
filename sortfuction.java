cd8d8cab00800a6918b27e41386dc73ee2948b70
vanshika-arya <vanshika3915@gmail.com>
2025-04-19 17:34:03 +0530

    sort elements of an Array by sort function

---
+++ sortfuction.java
@@ -0,0 +1,13 @@
+
+import java.util.Arrays;
+
+public class sortfuction {
+    public static void main(String[] args) {
+        int arr[]={34,12,78,98};
+        System.out.println(arr[0]);
+       Arrays.sort(arr);
+       System.out.println(arr[0]);
+    }
+    
+    
+}
