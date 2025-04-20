dc55f6eb6afea4829a14a8e10ce00098c096a769
vanshika-arya <vanshika3915@gmail.com>
2025-04-20 21:04:54 +0530

    copy of elements of an array

---
+++ pratice6.java
@@ -0,0 +1,14 @@
+public class pratice6 {
+    public static void main(String[] args) {
+        int arr1[]={16,17,18};
+        int arr2[]= new int[arr1.length];
+        for(int i=0;i<arr1.length;i++){
+            arr2[i]=arr1[i];
+        }
+        for(int j=0;j<arr2.length;j++){
+            System.out.println(arr2[j]);
+        }
+    }
+
+    
+}
