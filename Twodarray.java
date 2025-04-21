71e7256d3e5e5e60dd544809b2c94c3e657a1454
vanshika-arya <vanshika3915@gmail.com>
2025-04-21 13:53:52 +0530

    string basics

--- Twodarray.java
+++ Twodarray.java
@@ -2,11 +2,31 @@ public class Twodarray {
     public static void main(String[] args) {
         int a[][]={{1,2,3,4},{2,3,4,5}};
         int totalcount=0;
+        
+        
         for(int i=0;i<a.length;i++){
-            totalcount=totalcount+a[i].length;
-        }
-        System.out.println(totalcount);
+            
+            for(int j=0;j<a[i].length;j++){
+                totalcount++;
             }
+        }
+        
+        System.out.println(totalcount);
+        int sa[]=new int[totalcount];
+        int k=0;
+
+        for(int i=0;i<a.length;i++){
+            
+            for(int j=0;j<a[i].length;j++){
+                sa[k++]=a[i][j];
+            }
+        }
+
+        System.out.println("-------------------");
+        for(k=0;k<totalcount;k++)
+        System.out.println(sa[k]);
+            }
+            
         }
     
 
---
+++ string1.java
@@ -0,0 +1,11 @@
+public class string1 {
+    public static void main(String[] args) {
+        String s="vanshika";
+      System.out.println(s.charAt(0));
+      System.out.println(s.charAt(1));
+      System.out.println(s.charAt(2));
+      System.out.println(s);
+      System.out.println(s.length());
+    }
+    
+}
