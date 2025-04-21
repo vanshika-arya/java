378983de0fadfd522cca922c608773dc7c2a6f15
vanshika-arya <vanshika3915@gmail.com>
2025-04-21 12:49:53 +0530

    two array elements count

---
+++ Twodarray.java
@@ -0,0 +1,12 @@
+public class Twodarray {
+    public static void main(String[] args) {
+        int a[][]={{1,2,3,4},{2,3,4,5}};
+        int totalcount=0;
+        for(int i=0;i<a.length;i++){
+            totalcount=totalcount+a[i].length;
+        }
+        System.out.println(totalcount);
+            }
+        }
+    
+
