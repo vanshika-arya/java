1c5cdd282dc6a2dea7eb04722e1c90b0e71536ce
vanshika-arya <vanshika3915@gmail.com>
2025-04-21 14:05:46 +0530

    reverse of string

---
+++ reverseofstring.java
@@ -0,0 +1,9 @@
+public class reverseofstring {
+    public static void main(String[] args) {
+        String s="Vanshika";
+       for(int i=s.length()-1;i>=0;i--)
+            System.out.println(s.charAt(i));
+        }
+    }
+    
+
