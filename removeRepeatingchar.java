f23eac31d088603b75b33cc989a5517fa58be8d7
vanshika-arya <vanshika3915@gmail.com>
2025-04-23 07:58:57 +0530

    remove repeating character

---
+++ removeRepeatingchar.java
@@ -0,0 +1,12 @@
+public class removeRepeatingchar {
+    public static void main(String[] args) {
+        String s="apple";
+        char arr[]=s.toCharArray();
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+            if(arr[i]==arr[i+1])
+            i++;
+        }
+    }
+    
+}
