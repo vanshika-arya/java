c7a9d8cf2b0cb6cabb0a3f4e8390bcd0cfb89c41
vanshika-arya <vanshika3915@gmail.com>
2025-04-24 21:57:08 +0530

    questions pratice

---
+++ pratice7.java
@@ -0,0 +1,9 @@
+public class pratice7 {
+    public static void main(String[] args) {
+        for(int i=0;i<1;i++)
+            for(int j=0;j<1;j++);
+            System.out.println("hii");
+        
+    }
+    
+}
