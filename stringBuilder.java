c0963c42cc0b1035961619c440b8ceea05032de7
vanshika-arya <vanshika3915@gmail.com>
2025-04-25 10:20:42 +0530

    String Builder

---
+++ stringBuilder.java
@@ -0,0 +1,9 @@
+public class stringBuilder {
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder(13);
+       str.append("hello hii vanshika arya");
+       System.out.println(str.length());
+       System.out.println(str.capacity());
+    }
+    
+}
