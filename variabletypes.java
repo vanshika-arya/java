53348dbd6f155f2b0fb52e9f56be6a62fa6eb7d6
vanshika-arya <vanshika3915@gmail.com>
2025-04-25 12:32:59 +0530

    variable types

---
+++ variabletypes.java
@@ -0,0 +1,13 @@
+public class variabletypes {
+    int a=12;//instance variable
+    static int b=10; //ststic variables
+    public static void main(String[] args) {
+        int c=11;//local variable
+        System.out.println(c);//prints the local variable value inside the method where it declared
+        variabletypes obj = new variabletypes();
+        System.out.println(obj.a);//prints instance variable---> by creating an object
+        System.out.println(b);//prints static variable
+        System.out.println(variabletypes.b);//prints static variable
+    }
+    
+}
