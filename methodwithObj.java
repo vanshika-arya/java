6caf3095af420626c5de24a1c2a0b4734d477009
vanshika-arya <vanshika3915@gmail.com>
2025-04-25 10:50:40 +0530

    create method by making obj in main method

---
+++ methodwithObj.java
@@ -0,0 +1,10 @@
+public class methodwithObj {
+    void hello(){
+        System.out.println("Hello");
+    }
+    public static void main(String[] args) {
+        methodwithObj o = new methodwithObj();
+        o.hello();
+    }
+    
+}
