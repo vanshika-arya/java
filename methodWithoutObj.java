bcd5125a5a18e8b297c7f629924cb35235c382fe
vanshika-arya <vanshika3915@gmail.com>
2025-04-25 10:47:20 +0530

    create method in java without creating an object

---
+++ methodWithoutObj.java
@@ -0,0 +1,11 @@
+public class methodWithoutObj {
+  static void hello(){
+    System.out.println("hellooooo");
+  }
+  public static void main(String[] args) {
+    hello();
+
+    
+  }
+    
+}
--- stringBuilder.java
+++ stringBuilder.java
@@ -1,9 +1,13 @@
 public class stringBuilder {
     public static void main(String[] args) {
+        
         StringBuilder str = new StringBuilder(13);
        str.append("hello hii vanshika arya");
        System.out.println(str.length());
        System.out.println(str.capacity());
+       //if capacity of string exceed than tha capacity defined.
+       //capacity=(old capacity)*2+2;
+      
     }
     
 }
