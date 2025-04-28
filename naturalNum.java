b67561d347e63d4e58a5d94f0ceba7f973698b1a
vanshika-arya <vanshika3915@gmail.com>
2025-04-28 12:29:58 +0530

    natural number prints

---
+++ naturalNum.java
@@ -0,0 +1,11 @@
+public class naturalNum {
+    static void natural_num(int n){
+        for(int i=1;i<=n;i++){
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args) {
+        natural_num(7);
+    }
+    
+}
