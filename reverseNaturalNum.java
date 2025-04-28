e0611ccc760fedbc4411fab9aab9af7bb98cd074
vanshika-arya <vanshika3915@gmail.com>
2025-04-28 12:45:26 +0530

    pratice quetions

---
+++ reverseNaturalNum.java
@@ -0,0 +1,11 @@
+public class reverseNaturalNum {
+    static void reverseNaturalNum(int n){
+        for(int i=n;i>=1;i--){
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args) {
+        reverseNaturalNum(6);
+    }
+    
+}
