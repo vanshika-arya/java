1ef12766670bbc05e761e81328872de3c5d1608e
vanshika-arya <vanshika3915@gmail.com>
2025-05-23 10:25:07 +0530

    get function of array list with try catch block

---
+++ ArrayList2.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+public class ArrayList2 {
+   
+
+    public static void main(String[] args) {
+       ArrayList <String> arr= new ArrayList<>();
+        arr.add("Vanshika");
+        arr.add("Alshifa");
+        arr.add(0, " Jyoti");
+        System.out.println(arr);
+        try{
+        System.out.println(arr.get(4));
+        }catch(Exception e){
+            System.out.println("array index out of bound");
+        }finally{
+       arr.add(3, "Rashmi");
+        System.out.println(arr.get(3));
+        }
+    }
+    
+}
+
+
