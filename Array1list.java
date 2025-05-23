c7a887cb9d9cdc6830299a5d4cb387ca5547f4f2
vanshika-arya <vanshika3915@gmail.com>
2025-05-23 10:14:50 +0530

    array list add Function

---
+++ Array1list.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Array1list {
+    public static void main(String[] args) {
+       ArrayList <String> arr= new ArrayList<>();
+        arr.add("Vanshika");
+        arr.add("Alshifa");
+        arr.add(0, " Jyoti");
+        System.out.println(arr);
+    }
+    
+}
