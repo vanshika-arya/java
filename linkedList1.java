d0b67e281e87a6b14ddf30030dc00b69671f7a5c
vanshika-arya <vanshika3915@gmail.com>
2025-05-27 10:34:44 +0530

    Linked list basics

---
+++ linkedList1.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+public class linkedList1{
+    public static void main(String[] args) {
+        LinkedList<Integer> arr= new LinkedList<>();
+        long start=System.nanoTime();
+        arr.add(23);
+        arr.add(54);
+        arr.add(11);
+        System.out.println(arr.contains(34));
+        System.out.println(arr.contains(11));
+        System.out.println(arr.remove(0));
+        System.out.println(arr);
+        long end = System.nanoTime();
+        arr.set(0,5);
+        System.out.println(end-start);       
+    }
+    
+}
