18f82ea1b53dce5c3111f2610ef2adc76a2b4831
vanshika-arya <vanshika3915@gmail.com>
2025-05-27 11:17:43 +0530

    remove repeated elements using HashSet

--- set.java
+++ set.java
@@ -2,15 +2,12 @@ import java.util.HashSet;
 import java.util.TreeSet;
 public class set {
     public static void main(String[] args) {
-        HashSet<String> set =new HashSet<>();
-        set.add("vanshika");
-        set.add("alshifa");
-        System.out.println(set);
-        TreeSet <String> t= new TreeSet<>();
-        t.add("v");
-        t.add("a");
-        t.add("V");
-        System.out.println(t);
+        HashSet<Integer> set =new HashSet<>();
+        int a[]={1,2,2,42,4,5,3,53,45,3,5};
+       for(int i=0;i<a.length;i++){
+        set.add(a[i]);
+       }
+       System.out.println(set);
     }
     
 }
