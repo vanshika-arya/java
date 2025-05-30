1a2223ff9e4ca77ac8d99bb5669d161d54d32423
vanshika-arya <vanshika3915@gmail.com>
2025-05-30 12:56:35 +0530

    listIterator

---
+++ listIterator.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class listIterator {
+    public static void main(String[] args) {
+        ArrayList<String> list= new ArrayList<>();
+        list.add("Vanshika");
+        list.add("anjali");
+        list.add("astha");
+        list.add("alishifa");
+        list.add("Jyoti");
+        ListIterator<String> it= list.listIterator();
+        while(it.hasNext()){
+            String data= it.next();
+            System.out.println(data);
+        }
+        System.out.println("Ulta");
+        while(it.hasPrevious()){
+            String PrevData=it.previous();
+            System.out.println(PrevData);
+        }
+    }
+    
+}
