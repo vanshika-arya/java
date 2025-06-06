fba7ffa26a9c89f8fcd613a2b5e4132d6a2b2ef6
vanshika-arya <vanshika3915@gmail.com>
2025-06-06 10:47:23 +0530

    map + filter API

---
+++ mapStream.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.*;
+public class mapStream {
+    public static void main(String[] args) {
+        List <Integer> list= Arrays.asList(10,30,87,6);
+        List <Integer> evenlist=list.stream()
+                                .filter(n-> n%2==0)
+                              .map(n->n*5)
+                                .collect(Collectors.toList());
+                  System.out.println(evenlist);     
+    }
+    
+}
