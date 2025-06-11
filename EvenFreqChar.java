ba2315d7f9963f50c7a35f438a1add2313f4b5e1
vanshika-arya <vanshika3915@gmail.com>
2025-06-11 23:20:43 +0530

    counting the number of characters whose frequency are even

---
+++ EvenFreqChar.java
@@ -0,0 +1,20 @@
+public class EvenFreqChar {
+    public static void main(String[] args) {
+         String s = "aabbccd";
+        int[] freq = new int[26];
+        
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        int count = 0;
+        for (int f : freq) {
+            if (f > 0 && f % 2 == 0) {
+                count++;
+            }
+        }
+
+        System.out.println("Characters with even frequency: " + count);
+    }
+    
+}
