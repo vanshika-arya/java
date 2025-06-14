4a7bd4f9a5905b39062c5b589bfe126e237b635b
vanshika-arya <vanshika3915@gmail.com>
2025-06-14 21:49:30 +0530

    Two String Rotation of Each other or not

---
+++ StringRotation.java
@@ -0,0 +1,18 @@
+public class StringRotation {
+    public static void main(String[] args) {
+        String s1 = "ABCD";
+        String s2 = "CDAAB";
+
+        if (s1.length() != s2.length()) {
+            System.out.println("Not Rotation");
+        } else {
+            String s3 = s1 + s1;
+
+            if (s3.contains(s2)) {
+                System.out.println("Rotation");
+            } else {
+                System.out.println("Not Rotation");
+            }
+        }
+    }
+}
